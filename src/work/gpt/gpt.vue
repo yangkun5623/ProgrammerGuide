3710408c35ccce57896617c344170c0cc5b056b1
shieru <122915623@qq.com>
2023-12-26 01:48:54 +0800

    add: Bug

--- src/work/gpt/gpt.vue
+++ src/work/gpt/gpt.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <iframe style="height: calc(100vh - 100px);width: 100%;border: none" src="https://chat-gpt-next-web-3e4h.vercel.app"/>
+  <iframe style="height: calc(100vh - 100px);width: 100%;border: none" src="https://www.shieru.info/"/>
 </template>
 
 <style scoped>
