6102b6f9b93e0dee94619391cd36aff8e26d6f2f
shieru <122915623@qq.com>
2023-12-25 17:37:23 +0800

    add: 布局初步

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import Test from "@/components/test.vue";
 import {ref} from "vue";
 import zhCN from 'ant-design-vue/es/locale/zh_CN';
 import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';
+import Layout from "@/core/layout/layout.vue";
 dayjs.locale('zh-cn');
 
 const theme = ref({
@@ -16,35 +16,9 @@ const locale = ref(zhCN);
 
 <template>
   <a-config-provider :locale="locale" :theme="theme">
-      <test/>
+     <layout/>
   </a-config-provider>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/core/layout/layout.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+import headerNav from "@/core/layout/part/headerNav.vue";
+import LeftMenu from "@/core/layout/part/leftMenu.vue";
+import type { CSSProperties } from 'vue';
+const headerStyle: CSSProperties = {
+  color: '#fff',
+  height: 48,
+  paddingInline: 0,
+  lineHeight: '48px',
+  backgroundColor: 'white',
+};
+
+const contentStyle: CSSProperties = {
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: 'white',
+};
+
+const siderStyle: CSSProperties = {
+  lineHeight: '64px',
+  color: '#fff',
+  height: '100%',
+  backgroundColor: 'white',
+};
+
+const footerStyle: CSSProperties = {
+  color: '#fff',
+  backgroundColor: 'white',
+};
+</script>
+
+<template>
+  <a-layout>
+    <a-layout-sider :style="siderStyle">
+      <LeftMenu/>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header :style="headerStyle">
+        <headerNav/>
+      </a-layout-header>
+      <a-layout-content :style="contentStyle">Content</a-layout-content>
+      <a-layout-footer :style="footerStyle">Footer</a-layout-footer>
+    </a-layout>
+  </a-layout>
+</template>
+
+<style scoped>
+
+</style>
