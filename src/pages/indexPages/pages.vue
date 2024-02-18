59907ebe9c1fb4b78c3282c422de8bbe1cb73068
shieru <122915623@qq.com>
2024-02-18 17:40:48 +0800

    fix: 添加pdf导出（。。。）

--- src/pages/indexPages/pages.vue
+++ src/pages/indexPages/pages.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import {onMounted, ref} from "vue";
+import {htmlToPDF} from "@/utils/exportPdf";
+const page = ref()
+onMounted(() => {
+  setTimeout(() => {
 
+
+    console.log('page', page.value)
+    htmlToPDF(page.value.$el, '测试')
+  }, 1000)
+})
 </script>
 
 <template>
-    <RouterView/>
+    <RouterView ref="page"/>
 </template>
 
 <style scoped lang="less">
---
+++ src/utils/exportPdf.ts
@@ -0,0 +1,36 @@
+import html2canvas from "html2canvas";
+import * as jspdf from "jspdf";
+
+
+export const htmlToPDF = async (pdfDom: HTMLElement, title: string = "标题", bgColor = "#fff") => {
+    pdfDom.style.padding = '0 10px !important'
+    const A4Width = 595.28;
+    const A4Height = 841.89;
+    let canvas = await html2canvas(pdfDom, {
+        scale: 2,
+        useCORS: true,
+        backgroundColor: bgColor,
+    });
+    let pageHeight = (canvas.width / A4Width) * A4Height;
+    let leftHeight = canvas.height;
+    let position = 0;
+    let imgWidth = A4Width;
+    let imgHeight = (A4Width / canvas.width) * canvas.height;
+    /*
+       根据自身业务需求  是否在此处键入下方水印代码
+      */
+    let pageData = canvas.toDataURL("image/jpeg", 1.0);
+    console.log('pageData', pageData)
+    let PDF = new jspdf("p", 'pt', 'a4');
+    if (leftHeight < pageHeight) {
+        PDF.addImage(pageData, "JPEG", 0, 0, imgWidth, imgHeight);
+    } else {
+        while (leftHeight > 0) {
+            PDF.addImage(pageData, "JPEG", 0, position, imgWidth, imgHeight);
+            leftHeight -= pageHeight;
+            position -= A4Height;
+            if (leftHeight > 0) PDF.addPage();
+        }
+    }
+    PDF.save(title + ".pdf");
+}
