a36fd51fbcc905bb0bcc97b7817e16371a7ba2dc
shieru <122915623@qq.com>
2024-11-01 17:32:24 +0800

    add: 完善

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,65 +1,48 @@
-<script lang="ts" setup>
-import type { CSSProperties } from 'vue';
-import { ref} from 'vue'
-import CardAuthorTable from "@/pages/worktable/components/Cards/CardAuthorTable.vue";
-import CardBarChart from "@/pages/worktable/components/Cards/CardBarChart.vue";
-import CardBillingInfo from "@/pages/worktable/components/Cards/CardBillingInfo.vue";
-const contentStyle: CSSProperties = {
-  textAlign: 'center',
-  minHeight: 120,
-  lineHeight: '120px',
-  color: '#fff',
-};
-
-const footerStyle: CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
-  backgroundColor: '#fff',
-};
-const testLayout:any = ref([
-  {"x":0,"y":0,"w":100,"h":39,"i":"0", component: CardAuthorTable},
-  {"x":50,"y":39,"w":50,"h":40,"i":"1", component: CardBarChart},
-  {"x":0,"y":39,"w":50,"h":48,"i":"2", component: CardBillingInfo},
-  {"x":50,"y":69,"w":50,"h":25,"i":"3"}
-])
-
-</script>
 <template>
-  <a-layout>
-    <a-layout-content :style="contentStyle">
-      <grid-layout
-          :layout.sync="testLayout"
-          :col-num="100"
-          :row-height="2"
-          :is-draggable="true"
-          :is-resizable="true"
-          :is-mirrored="false"
-          :vertical-compact="true"
-          :margin="[10, 10]"
-          :use-css-transforms="true"
-      >
-
-        <grid-item v-for="item in testLayout"
-                   :x="item.x"
-                   style="background: white"
-                   :y="item.y"
-                   :w="item.w"
-                   :h="item.h"
-                   :i="item.i"
-                   :key="item.i">
-          <component style="height: 100%" :is="item.component"/>
-        </grid-item>
-      </grid-layout>
-    </a-layout-content>
-    <a-layout-footer :style="footerStyle">
-      <a-typography-link href="https://jbaysolutions.github.io/vue-grid-layout/zh/guide/#npm" target="_blank">
-        VueGridLayout
-      </a-typography-link>
-    </a-layout-footer>
-  </a-layout>
-
+  <a-typography style="background: white;padding: 6px">
+    <a-typography-title>网页工具</a-typography-title>
+    <a-typography-title :level="3">在线工具</a-typography-title>
+    <a-typography-paragraph>
+      <a-space>
+        <template #split>
+          <a-divider type="vertical" />
+        </template>
+        <a-typography-link href="https://www.aigei.com/bgremover" target="_blank">
+          在线图片去底工具
+        </a-typography-link>
+        <a-typography-link href="https://tool.lu/timestamp/" target="_blank">
+          时间戳转换工具
+        </a-typography-link>
+        <a-typography-link href="https://datav.aliyun.com/portal/school/atlas/area_selector" target="_blank">
+          数据可视化工具
+        </a-typography-link>
+        <a-typography-link href="https://astexplorer.net/" target="_blank">
+          AST分析工具
+        </a-typography-link>
+        <a-typography-link href="https://unpkg.com/" target="_blank">
+          UNPKG
+        </a-typography-link>
+        <a-typography-link href="http://www.codebaoku.com/" target="_blank">
+          编程宝库
+        </a-typography-link>
+      </a-space>
+    </a-typography-paragraph>
+    <a-typography-title :level="3">在线工具合集</a-typography-title>
+    <a-typography-paragraph>
+      <a-space>
+        <template #split>
+          <a-divider type="vertical" />
+        </template>
+        <a-typography-link href="https://uutool.cn/type/new" target="_blank">
+          UU在线工具
+        </a-typography-link>
+        <a-typography-link href="https://ss.netnr.com/" target="_blank">
+          Netnr
+        </a-typography-link>
+      </a-space>
+    </a-typography-paragraph>
+  </a-typography>
 </template>
-<style>
-</style>
-
+<script lang="ts" setup>
+</script>
 
