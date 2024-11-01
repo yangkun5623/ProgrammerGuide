2d38c735d85bed7005d143d4b6321f03f828cd5a
shieru <122915623@qq.com>
2024-11-01 17:42:01 +0800

    add: 完善

--- src/pages/worktable/components/Cards/CardAuthorTable.vue
+++ src/pages/worktable/components/Cards/CardAuthorTable.vue
@@ -133,35 +133,6 @@ const data:any = ref([
 		</template>
 		<a-table :columns="columns" :data-source="data" :pagination="false">
 
-			<template slot="author" slot-scope="author">
-				<div class="table-avatar-info">
-					<a-avatar shape="square" :src="author.avatar" />
-					<div class="avatar-info">
-						<h6>{{ author.name }}</h6>
-						<p>{{ author.email }}</p>
-					</div>
-				</div>
-			</template>
-
-			<template slot="func" slot-scope="func">
-				<div class="author-info">
-					<h6 class="m-0">{{ func.job }}</h6>
-					<p class="m-0 font-regular text-muted">{{ func.department }}</p>
-				</div>
-			</template>
-
-			<template slot="status" slot-scope="status">
-				<a-tag class="tag-status" :class="status ? 'ant-tag-primary' : 'ant-tag-muted'">
-					{{ status ? "ONLINE" : "OFFLINE" }}
-				</a-tag>
-			</template>
-
-			<template slot="editBtn" slot-scope="row">
-				<a-button type="link" :data-id="row.key" class="btn-edit">
-					Edit
-				</a-button>
-			</template>
-
 		</a-table>
 	</a-card>
 </template>
